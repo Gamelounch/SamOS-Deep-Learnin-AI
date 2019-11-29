fc52215b5614321fa7d271bddcf28a4f26b9cb6f
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2019-11-29 19:05:49 +0100

    added Sample Data
    Added Sample Data for Datatable usage

--- Plugins/Picture Recognition/PAFRSW.py
+++ Plugins/Picture Recognition/PAFRSW.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
 #                                0
-img = cv2.imread('image.jpg',cv2.IMREAD_GRAYSCALE)
-#IMREAD_COLOR = 1
-#IMREAD_UNCHANGED = -1
+img = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+# IMREAD_COLOR = 1
+# IMREAD_UNCHANGED = -1
 
 cv2.imshow('image', img)
 cv2.waitkey(0)
 cv2.destroyallwindows()
-
