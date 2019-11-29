38defdd76c5765c68a4eae11d13176d2d349abb9
Gamelounch <58276955+Gamelounch@users.noreply.github.com>
2019-11-29 19:45:58 +0100

    Database Update
    Added Database Functions for Data that we gonna use later on

--- Plugins/database/chat_database.py
+++ Plugins/database/chat_database.py
@@ -11,4 +11,11 @@ sql_transaction = []
 
 connection = sqlite3.connect('{}.db' .format(timeframe))
 c = connection.cursor()
+#This will Create a New Database Table if not Exist
+def create_table():
+    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply
+    (parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT,
+    comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
+if __name__ == "__main__":
+    create_table()
